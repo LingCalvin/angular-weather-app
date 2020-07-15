8c95033fa8afe7b5d7e458cee00f4ce808269b62
Calvin Ling <calvinling8@gmail.com>
2020-07-15 02:53:16 -0400

    Add third-party licenses to about page

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,14 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css'],
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
   appVersion =
     'VERSION' in environment
       ? (environment as { VERSION: string }).VERSION
       : 'DEBUG';
+
+  licenses!: Observable<string>;
+
+  getLicenses(): void {
+    this.licenses = this.http.get('/3rdpartylicenses.txt', {
+      responseType: 'text',
+    });
+  }
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getLicenses();
+  }
 }
